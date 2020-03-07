080a59f385c0db88f621039fd01fcd79a6ba0ea5
SpaceCode <vladlen.beilik@gmail.com>
2020-03-07 17:37:14 +0200

    release 1.0.41 * fixed migration

--- database/migrations/2020_01_01_000000_update_users_table.php
+++ database/migrations/2020_01_01_000000_update_users_table.php
@@ -14,15 +14,19 @@ class UpdateUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
+
             if(!Schema::hasColumn('users', 'apps')) {
                 $table->longText('apps')->nullable()->after('name');
             }
+
             if(!Schema::hasColumn('users', 'fullName')) {
                 $table->string('fullName')->nullable()->after('name');
             }
+
             if(!Schema::hasColumn('users', 'avatar')) {
                 $table->string('avatar')->nullable()->after('name');
             }
+
         });
     }
 
