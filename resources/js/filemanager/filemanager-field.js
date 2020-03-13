c6c843e2f77991d2cfebd17cb30af9838b6291e2
SpaceCode <vladlen.beilik@gmail.com>
2020-03-13 17:57:50 +0200

    Delete filemanager-field.js

--- resources/js/filemanager/filemanager-field.js
+++
@@ -1,5 +0,0 @@
-Nova.booting(Vue => {
-    Vue.component('index-filemanager-field', require('./field/IndexField'));
-    Vue.component('detail-filemanager-field', require('./field/DetailField'));
-    Vue.component('form-filemanager-field', require('./field/FormField'));
-});
