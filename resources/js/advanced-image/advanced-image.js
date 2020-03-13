574110c4afe6733e9fb3ec47c962488403db6fcb
SpaceCode <vladlen.beilik@gmail.com>
2020-03-13 17:53:49 +0200

    Delete advanced-image.js

--- resources/js/advanced-image/advanced-image.js
+++
@@ -1,9 +0,0 @@
-import IndexField from './components/IndexField'
-import DetailField from './components/DetailField'
-import FormField from './components/FormField'
-
-Nova.booting((Vue) => {
-    Vue.component('index-advanced-image-field', IndexField);
-    Vue.component('detail-advanced-image-field', DetailField);
-    Vue.component('form-advanced-image-field', FormField);
-});
