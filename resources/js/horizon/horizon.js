46aac28d0bdcaa5cb98765909de955a8914869f1
SpaceCode <vladlen.beilik@gmail.com>
2020-03-13 17:58:11 +0200

    Delete horizon.js

--- resources/js/horizon/horizon.js
+++
@@ -1,9 +0,0 @@
-Nova.booting((Vue, router, store) => {
-    router.addRoutes([
-        {
-            name: 'maia-horizon',
-            path: '/horizon',
-            component: require('./components/Tool').default,
-        },
-    ]);
-});
